877dfd6c43f91c1a50d42ec1c5959843952b6eed
Perumalla Dharan <opdharanff@gmail.com>
2023-09-06 10:20:23 +0530

    Devu_and_Grapes

--- SEM-5/6th_September/Devu_and_Grapes.cpp
+++ SEM-5/6th_September/Devu_and_Grapes.cpp
@@ -43,3 +43,26 @@
 
 #include <iostream>
 using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int arr[n];
+        int count=0;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+            if(arr[i]%k!=0){
+                if(arr[i]>k){
+                    count+=min(arr[i]%k,k-(arr[i]%k));
+                }
+                else{
+                    count+=k-arr[i];
+                }
+            }
+        }
+        cout<<count<<endl;
+    }
+}
